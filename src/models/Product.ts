85d2fc3660977a9a9fe18f935b1bf72940381170
Alex Vieyra <vieyrafc@gmail.com>
2023-02-23 10:21:54 -0600

    Updated api endpoints

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,9 +1,9 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 export const ProductSchema = Yup.object({
   id: Yup.string(),
-  title: Yup.string().required().default(""),
-  description: Yup.string().default(""),
+  title: Yup.string().required().default(''),
+  description: Yup.string().default(''),
   price: Yup.number().positive().required().defined().default(0),
 });
 
@@ -13,3 +13,6 @@ export const AvailableProductSchema = ProductSchema.shape({
 
 export type Product = Yup.InferType<typeof ProductSchema>;
 export type AvailableProduct = Yup.InferType<typeof AvailableProductSchema>;
+export type ResponseProducts = {
+  data: Array<AvailableProduct>;
+};
