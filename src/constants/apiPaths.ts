9a1ae4da671d9294d5f59dc243105b4998716c5b
Alex Vieyra <vieyrafc@gmail.com>
2023-03-23 14:03:36 -0600

    Task 5
    - Updated file upload endpoint
    - Updated product creation endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://ui3t64lo4h.execute-api.us-east-2.amazonaws.com/dev',
-  order: 'https://ui3t64lo4h.execute-api.us-east-2.amazonaws.com/dev',
-  import: 'https://ui3t64lo4h.execute-api.us-east-2.amazonaws.com/dev',
+  product: 'https://.execute-api.us-east-2.amazonaws.com/dev',
+  order: 'https://.execute-api.us-east-2.amazonaws.com/dev',
+  import: 'https://uepb4rehl2.execute-api.us-east-2.amazonaws.com/dev',
   bff: 'https://ui3t64lo4h.execute-api.us-east-2.amazonaws.com/dev',
-  cart: 'https://ui3t64lo4h.execute-api.us-east-2.amazonaws.com/dev',
+  cart: 'https://.execute-api.us-east-2.amazonaws.com/dev',
 };
 
 export default API_PATHS;
